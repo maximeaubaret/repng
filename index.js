1baf908a897b06568e1a6b218f4466960a8b87e1
Maxime Aubaret <maxime@moveyourbuddy.io>
2018-07-16 12:45:04 +0200

    Add puppeteer options

--- index.js
+++ index.js
@@ -1,96 +1,92 @@
-require('babel-register')({
-  plugins: [
-    'babel-plugin-transform-runtime'
-  ].map(require.resolve),
+require("babel-register")({
+  plugins: ["babel-plugin-transform-runtime"].map(require.resolve),
   presets: [
-    'babel-preset-env',
-    'babel-preset-stage-0',
-    'babel-preset-react'
+    "babel-preset-env",
+    "babel-preset-stage-0",
+    "babel-preset-react"
   ].map(require.resolve)
-})
+});
 
-const fs = require('fs')
-const puppeteer = require('puppeteer')
-const { Readable } = require('stream')
-const path = require('path')
-const { createElement: h } = require('react')
-const { renderToStaticMarkup } = require('react-dom/server')
-const Datauri = require('datauri')
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+const { Readable } = require("stream");
+const path = require("path");
+const { createElement: h } = require("react");
+const { renderToStaticMarkup } = require("react-dom/server");
+const Datauri = require("datauri");
 
-const baseCSS = `*{box-sizing:border-box}body{margin:0;font-family:system-ui,sans-serif}`
+const baseCSS = `*{box-sizing:border-box}body{margin:0;font-family:system-ui,sans-serif}`;
 
-const getHtmlData = ({
-  body,
-  baseCSS,
-  css,
-  webfont
-}) => {
-  const fontCSS = webfont ? getWebfontCSS(webfont) : ''
-  const html = `<!DOCTYPE html><meta charset="utf-8"><style>${baseCSS}${fontCSS}${css}</style>${body}`
-  const htmlBuffer = new Buffer(html, 'utf8')
-  const datauri = new Datauri()
-  datauri.format('.html', htmlBuffer)
-  const data = datauri.content
-  return data
-}
+const getHtmlData = ({ body, baseCSS, css, webfont }) => {
+  const fontCSS = webfont ? getWebfontCSS(webfont) : "";
+  const html = `<!DOCTYPE html><meta charset="utf-8"><style>${baseCSS}${fontCSS}${css}</style>${body}`;
+  const htmlBuffer = new Buffer(html, "utf8");
+  const datauri = new Datauri();
+  datauri.format(".html", htmlBuffer);
+  const data = datauri.content;
+  return data;
+};
 
-const getWebfontCSS = (fontpath) => {
-  const { content } = new Datauri(fontpath)
-  const [ name, ext ] = fontpath.split('/').slice(-1)[0].split('.')
-  const css = (`@font-face {
+const getWebfontCSS = fontpath => {
+  const { content } = new Datauri(fontpath);
+  const [name, ext] = fontpath
+    .split("/")
+    .slice(-1)[0]
+    .split(".");
+  const css = `@font-face {
   font-family: '${name}';
   font-style: normal;
   font-weight: 400;
   src: url(${content});
-}`)
-  return css
-}
+}`;
+  return css;
+};
 
 module.exports = async (Component, opts = {}) => {
   const {
     props = {},
-    css = '',
+    css = "",
     filename,
     outDir,
     width,
     height,
     scale = 1,
-    webfont
-  } = opts
+    webfont,
+    puppeteerOptions
+  } = opts;
 
-  const body = renderToStaticMarkup(h(Component, props))
+  const body = renderToStaticMarkup(h(Component, props));
 
   const data = getHtmlData({
     body,
     baseCSS,
     css,
     webfont
-  })
+  });
 
   // todo:
   // - scale
   // - delay
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto(data)
+  const browser = await puppeteer.launch(puppeteerOptions);
+  const page = await browser.newPage();
+  await page.goto(data);
   const result = await page.screenshot({
-    type: 'png',
+    type: "png",
     clip: {
       x: 0,
       y: 0,
       width: parseInt(width),
-      height: parseInt(height),
+      height: parseInt(height)
     },
     omitBackground: true
-  })
-  await browser.close()
+  });
+  await browser.close();
 
-  const stream = new Readable()
-  stream._read = () => {}
+  const stream = new Readable();
+  stream._read = () => {};
 
-  stream.push(result)
-  stream.push(null)
+  stream.push(result);
+  stream.push(null);
 
-
-  return stream
-}
+  return stream;
+};
